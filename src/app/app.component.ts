6913b9f1a47cfec236b1190dbc9e6e1eaf604a46
Vedavyas <62705605+vedavyastadkod@users.noreply.github.com>
2023-10-09 06:32:47 +0000

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -90,3 +90,19 @@ export class AppComponent {
     window.print();
   }
 }
+
+/*
+References -- 
+
+https://artskydj.github.io/jsPDF/docs/jsPDF.html  // official doc for jspdf
+
+https://medium.com/@berkayyyulguel/angular-convert-html-to-pdf-via-jspdf-8c63c8c61ad9  // example to convert html to pdf 
+
+https://phppot.com/javascript/jspdf-html-example/     // example of jspdf
+
+https://www.geeksforgeeks.org/how-to-generate-pdf-file-using-jspdf-library/    // how to create pdf with js
+
+
+https://shamaahlawat.medium.com/page-split-using-jspdf-and-html2canvas-in-react-js-while-downloading-pdf-6c75da960019    // page split using react and more jspdf method usage example
+
+*/
